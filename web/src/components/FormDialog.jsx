12d01106ee22aba0f2d946186133817542617bb9
dissBack-Ma <291258024@qq.com>
2023-08-23 14:04:17 +0900

    upd

--- web/src/components/FormDialog.jsx
+++ web/src/components/FormDialog.jsx
@@ -1,17 +1,16 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+/* eslint-disable no-console */
+import React, { useState } from 'react';
+import { Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+
+const ipRegex = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 
 export default function FormDialog() {
-  const [open, setOpen] = React.useState(false);
-  const [cameraIpAddress, setCameraIpAddress] = React.useState('');
-  const [cameraName, setCameraName] = React.useState('');
-  const [error, setError] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [ipAddress, setIpAddress] = useState('');
+  const [name, setName] = useState('');
+
+  const [ipError, setIpError] = useState(false);
+  const [nameError, setNameError] = useState(false);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -19,13 +18,43 @@ export default function FormDialog() {
 
   const handleClose = () => {
     setOpen(false);
+    setIpAddress('');
+    setName('');
+    setIpError(false);
+    setNameError(false);
   };
 
-  const handleSave = () => {
-    if (cameraIpAddress.trim() === '' || cameraName.trim() === '') {
-      setError(true);
+  const handleIpChange = (event) => {
+    const value = event.target.value;
+    setIpAddress(value);
+
+    if (ipRegex.test(value)) {
+      // 如果输入的 IP 地址格式正确，则取消错误提示
+      setIpError(false);
     } else {
-      setError(false);
+      // 否则，显示错误提示
+      setIpError(true);
+    }
+  };
+
+  const handleNameChange = (event) => {
+    const value = event.target.value;
+    setName(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const isIpInvalid = !ipRegex.test(ipAddress);
+    const isNameInvalid = name === '';
+
+    setIpError(isIpInvalid);
+    setNameError(isNameInvalid);
+
+    if (!isIpInvalid && !isNameInvalid) {
+      console.log('IP Address:', ipAddress);
+      console.log('Camera Name:', name);
+      // 在这里，你可以进一步处理表单的提交逻辑，例如发送给后端API等。
       handleClose();
     }
   };
@@ -37,47 +66,42 @@ export default function FormDialog() {
       </Button>
       <Dialog open={open} onClose={handleClose} fullWidth={true}>
         <DialogTitle>add new</DialogTitle>
-        <DialogContent>
-          <DialogContentText>add a new camera</DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="camera-ipaddress"
-            label="カメラIPアドレス"
-            fullWidth
-            variant="standard"
-            required
-            value={cameraIpAddress}
-            onChange={(event) => setCameraIpAddress(event.target.value)}
-            className={error && cameraIpAddress.trim() === '' ? 'error' : ''}
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="camera-name"
-            label="カメラ名"
-            fullWidth
-            variant="standard"
-            required
-            value={cameraName}
-            onChange={(event) => setCameraName(event.target.value)}
-            className={error && cameraName.trim() === '' ? 'error' : ''}
-          />
-          {error && <p style={{ color: 'red' }}>Please fill in all required fields.</p>}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleSave}>save</Button>
-        </DialogActions>
+        <form onSubmit={handleSubmit}>
+          <DialogContent>
+            <DialogContentText>add a new camera</DialogContentText>
+            <TextField
+              margin="dense"
+              id="camera-name"
+              label="Camera Name"
+              fullWidth
+              variant="standard"
+              required
+              value={name}
+              onChange={handleNameChange}
+              error={nameError}
+              helperText={nameError ? 'Name is required' : ''}
+            />
+            <TextField
+              margin="dense"
+              id="camera-ipaddress"
+              label="Camera IP Address"
+              fullWidth
+              variant="standard"
+              required
+              value={ipAddress}
+              onChange={handleIpChange}
+              error={ipError}
+              helperText={ipError ? 'IP format is wrong' : ''}
+            />
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button variant="contained" color="primary" type="submit" disabled={ipAddress === '' || name === ''}>
+                REGISTER
+              </Button>
+            </DialogActions>
+          </DialogContent>
+        </form>
       </Dialog>
-
-      <style>
-        {`
-            .error {
-              border: 2px solid red; // 设置红色边框
-            }
-          `}
-      </style>
     </div>
   );
 }
