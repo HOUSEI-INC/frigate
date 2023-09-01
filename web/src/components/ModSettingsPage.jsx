15ecfcbb35b096e5ffce5d013141f477297b0f67
dissBack-Ma <291258024@qq.com>
2023-09-01 14:13:28 +0900

    update

--- web/src/components/ModSettingsPage.jsx
+++ web/src/components/ModSettingsPage.jsx
@@ -15,26 +15,122 @@ import Slide from '@mui/material/Slide';
 import Switch from '@mui/material/Switch';
 import TextField from '@mui/material/TextField';
 import { FormControlLabel, Radio, RadioGroup, FormControl, FormLabel } from '@mui/material';
+import axios from 'axios';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export default function AlertDialog({ open, handleClose, name, config }) {
-  console.log(name);
-  console.log(config);
   const [state, setState] = React.useState({
     camChecked: false,
     detect: false,
+    detect_width: 0,
+    detect_height: 0,
+    detect_fps: 0,
     record: false,
+    record_retain_days: 0,
+    record_retain_mode: '',
+    event_pre_capture: 0,
+    event_post_capture: 0,
+    event_retain_days: 0,
+    event_retain_mode: '',
     snapshot: false,
     cleanCopy: false,
     timestamp: false,
+    snapshot_retain_days: 0,
+    snapshot_height: 0,
   });
+
+  React.useEffect(() => {
+    if (config) {
+      setState((prevState) => ({
+        ...prevState,
+        camChecked: config.enabled || false,
+        detect: config.detect.enabled || false,
+        detect_width: config.detect.width || 0,
+        detect_height: config.detect.height || 0,
+        detect_fps: config.detect.fps || 0,
+        record: config.record.enabled || false,
+        record_retain_days: config.record.retain.days || 0,
+        record_retain_mode: config.record.retain.mode || '',
+        event_pre_capture: config.record.events.pre_capture || 0,
+        event_post_capture: config.record.events.post_capture || 0,
+        event_retain_days: config.record.events.retain.default || 0,
+        event_retain_mode: config.record.events.retain.mode || '',
+        snapshot: config.snapshots.enabled || false,
+        cleanCopy: config.snapshots.clean_copy || false,
+        timestamp: config.snapshots.timestamp || false,
+        snapshot_retain_days: config.snapshots.retain.default || 0,
+        snapshot_height: config.snapshots.height || 0,
+      }));
+    }
+  }, [config]);
 
   const handleChange = (key) => (event) => {
     setState({ ...state, [key]: event.target.checked });
   };
+
+  const handleDataChange = (key) => (event) => {
+    setState({ ...state, [key]: event.target.value });
+  };
+
+  const handleSave = () => {
+    let data = {
+      cameras: {
+        [name]: {
+          enabled: state.camChecked,
+          detect: {
+            enabled: state.detect,
+            width: state.detect_width,
+            height: state.detect_height,
+            fps: state.detect_fps,
+          },
+          record: {
+            enabled: state.record,
+            retain: {
+              days: state.record_retain_days,
+              mode: state.record_retain_mode,
+            },
+            events: {
+              post_capture: state.event_post_capture,
+              pre_capture: state.event_pre_capture,
+              retain: {
+                default: state.event_retain_days,
+                mode: state.event_retain_mode,
+              },
+            },
+          },
+          snapshots: {
+            enabled: state.snapshot,
+            clean_copy: state.cleanCopy,
+            height: state.snapshot_height,
+            retain: {
+              default: state.snapshot_retain_days,
+            },
+            timestamp: state.timestamp,
+          },
+        },
+      },
+    };
+    console.log(data);
+    saveConfig(data);
+    handleClose();
+  };
+
+  const saveConfig = async (partial_config) => {
+    try {
+      const response = await axios.post(`config/partial/save?save_option=restart`, partial_config, {
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.status === 200) {
+        console.log('save success');
+      }
+    } catch (error) {
+      console.log('save filed');
+    }
+  };
+
   return (
     <div>
       <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
@@ -46,7 +142,7 @@ export default function AlertDialog({ open, handleClose, name, config }) {
             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
               {name} Settings
             </Typography>
-            <Button autoFocus color="inherit" onClick={handleClose}>
+            <Button autoFocus color="inherit" onClick={handleSave}>
               save
             </Button>
           </Toolbar>
@@ -72,11 +168,26 @@ export default function AlertDialog({ open, handleClose, name, config }) {
               />
               <br />
               width:
-              <TextField size="small" />
+              <TextField
+                size="small"
+                value={state.detect_width}
+                type="number"
+                onChange={handleDataChange('detect_width')}
+              />
               height:
-              <TextField size="small" />
+              <TextField
+                size="small"
+                type="number"
+                value={state.detect_height}
+                onChange={handleDataChange('detect_height')}
+              />
               fps:
-              <TextField size="small" />
+              <TextField
+                size="small"
+                type="number"
+                value={state.detect_fps}
+                onChange={handleDataChange('detect_fps')}
+              />
             </div>
           </ListItem>
           <Divider />
@@ -90,24 +201,52 @@ export default function AlertDialog({ open, handleClose, name, config }) {
                 inputProps={{ 'aria-label': 'controlled' }}
               />
               <br />
-              <TextField label="输入文本" variant="outlined" type="number" size="small" />
+              record_retain_days:
+              <TextField
+                variant="outlined"
+                type="number"
+                size="small"
+                value={state.record_retain_days}
+                onChange={handleDataChange('record_retain_days')}
+              />
               <br />
               <FormControl component="fieldset">
-                <FormLabel component="legend">选择项</FormLabel>
-                <RadioGroup row>
+                <FormLabel component="legend">record_retain_mode</FormLabel>
+                <RadioGroup row value={state.record_retain_mode} onChange={handleDataChange('record_retain_mode')}>
                   <FormControlLabel value="all" control={<Radio />} label="all" />
                   <FormControlLabel value="motion" control={<Radio />} label="motion" />
                   <FormControlLabel value="active_objects" control={<Radio />} label="active_objects" />
                 </RadioGroup>
               </FormControl>
               <br />
-              <TextField label="输入文本 1" variant="outlined" type="number" size="small" />
-              <TextField label="输入文本 2" variant="outlined" type="number" size="small" />
-              <TextField label="输入文本 3" variant="outlined" type="number" size="small" />
+              event_pre_capture:
+              <TextField
+                variant="outlined"
+                type="number"
+                size="small"
+                value={state.event_pre_capture}
+                onChange={handleDataChange('event_pre_capture')}
+              />
+              event_post_capture:
+              <TextField
+                variant="outlined"
+                type="number"
+                size="small"
+                value={state.event_post_capture}
+                onChange={handleDataChange('event_post_capture')}
+              />
+              event_retain_days:
+              <TextField
+                variant="outlined"
+                type="number"
+                size="small"
+                value={state.event_retain_days}
+                onChange={handleDataChange('event_retain_days')}
+              />
               <br />
               <FormControl component="fieldset">
-                <FormLabel component="legend">选择项2</FormLabel>
-                <RadioGroup row>
+                <FormLabel component="legend">event_retain_model</FormLabel>
+                <RadioGroup row value={state.event_retain_mode} onChange={handleDataChange('event_retain_mode')}>
                   <FormControlLabel value="all" control={<Radio />} label="all" />
                   <FormControlLabel value="motion" control={<Radio />} label="motion" />
                   <FormControlLabel value="active_objects" control={<Radio />} label="active_objects" />
@@ -126,17 +265,31 @@ export default function AlertDialog({ open, handleClose, name, config }) {
                 inputProps={{ 'aria-label': 'controlled' }}
               />
               <br />
+              clean_copy:
               <FormControlLabel
                 control={<Switch checked={state.cleanCopy} onChange={handleChange('cleanCopy')} color="primary" />}
-                label="clean_copy"
               />
+              timestamp:
               <FormControlLabel
                 control={<Switch checked={state.timestamp} onChange={handleChange('timestamp')} color="primary" />}
-                label="timestamp"
               />
               <br />
-              <TextField label="Input Field" variant="outlined" type="number" size="small" />
-              <TextField label="Input Field" variant="outlined" type="number" size="small" />
+              retain_days:
+              <TextField
+                variant="outlined"
+                type="number"
+                size="small"
+                value={state.snapshot_retain_days}
+                onChange={handleDataChange('snapshot_retain_days')}
+              />
+              height:
+              <TextField
+                onChange={handleDataChange('snapshot_height')}
+                variant="outlined"
+                type="number"
+                size="small"
+                value={state.snapshot_height}
+              />
             </div>
           </ListItem>
           <Divider />
